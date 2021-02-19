1dc22b5a5511b5b1920f8d3244d2a939f3a0340f
Jack Winfield <platocrat@tuta.io>
2021-02-19 15:48:51 -0600

    Added docs from data-transport-layer

--- src/common/misc.ts
+++ src/common/misc.ts
@@ -1,3 +1,7 @@
+/**
+ * Basic timeout-based async sleep function.
+ * @param ms Number of milliseconds to sleep.
+ */
 export const sleep = async (ms: number): Promise<void> => {
   return new Promise<void>((resolve, reject) => {
     setTimeout(() => {
