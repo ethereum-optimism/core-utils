b1a9b0265e7ef0420586191df9f9cf561475ae73
Jack Winfield <platocrat@tuta.io>
2021-02-22 16:28:03 -0600

    Always run yarn lint:fix after changes

--- src/common/common.ts
+++ src/common/common.ts
@@ -3,10 +3,10 @@ import { toHexString } from './hex-strings'
 export const assert = (condition: () => boolean, reason?: string) => {
   try {
     if (condition() === false) {
-      throw new Error(`Assertion failed: ${ reason }`)
+      throw new Error(`Assertion failed: ${reason}`)
     }
   } catch (err) {
-    throw new Error(`Assertion failed: ${ reason }\n${ err }`)
+    throw new Error(`Assertion failed: ${reason}\n${err}`)
   }
 }
 
