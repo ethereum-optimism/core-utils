67e60168940ec64b4c4afe2dd200e56885932bfe
Mark Tyneway <mark.tyneway@gmail.com>
2021-02-24 11:13:41 -0800

    fix: gas price encoding

--- test/coders/batch-encoder.spec.ts
+++ test/coders/batch-encoder.spec.ts
@@ -19,7 +19,7 @@ describe('BatchEncoder', () => {
           s: '0x' + '22'.repeat(32),
         },
         gasLimit: 500,
-        gasPrice: 100,
+        gasPrice: 1000000,
         nonce: 100,
         target: '0x' + '12'.repeat(20),
         data: '0x' + '99'.repeat(10),
@@ -28,6 +28,31 @@ describe('BatchEncoder', () => {
       const encoded = ctcCoder.eip155TxData.encode(eip155TxData)
       const decoded = ctcCoder.eip155TxData.decode(encoded)
       expect(eip155TxData).to.deep.equal(decoded)
+    })
+
+    it('should fail encoding a bad gas price', () => {
+      const badGasPrice = 1000001
+      const eip155TxData = {
+        sig: {
+          v: 1,
+          r: '0x' + '11'.repeat(32),
+          s: '0x' + '22'.repeat(32),
+        },
+        gasLimit: 500,
+        gasPrice: badGasPrice,
+        nonce: 100,
+        target: '0x' + '12'.repeat(20),
+        data: '0x' + '99'.repeat(10),
+        type: TxType.EIP155,
+      }
+
+      let error
+      try {
+        ctcCoder.eip155TxData.encode(eip155TxData)
+      } catch (e) {
+        error = e
+      }
+      expect(error.message).to.equal(`Gas Price ${badGasPrice} cannot be encoded`)
     })
   })
 
@@ -73,7 +98,7 @@ describe('BatchEncoder', () => {
           s: '0x' + '22'.repeat(32),
         },
         gasLimit: 500,
-        gasPrice: 100,
+        gasPrice: 1000000,
         nonce: 100,
         target: '0x' + '12'.repeat(20),
         data: '0x' + '99'.repeat(10),
