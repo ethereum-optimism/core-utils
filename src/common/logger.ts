41b1331744678f797b98b4bfcb3e258f810705c9
Annie Ke <annieke8@gmail.com>
2021-03-01 12:10:34 -0800

    [Refactor] Merge logger with service-base logger (#13)
    * [Refactor] Merge logger with base-service logger
    
    * bump version
    
    * backwards compatibility

--- src/common/logger.ts
+++ src/common/logger.ts
@@ -1,16 +1,92 @@
 /* Imports: External */
 import debug from 'debug'
+import colors from 'colors/safe'
 
-export const LOG_NEWLINE_STRING = process.env.LOG_NEW_LINES ? '\n' : ' <\\n> '
+/**
+ * A small logging utility.
+ */
+export class Logger {
+  /**
+   * @param namespace Namespace to attribute logs to.
+   */
+  constructor(public namespace: string) {}
 
-export interface Logger {
-  debug: (...args: any[]) => any
-  info: (...args: any[]) => any
-  warn: (...args: any[]) => any
-  error: (...args: any[]) => any
+  /**
+   * For printing basic informational logs.
+   * @param message Message to print.
+   */
+  public info(message: string): void {
+    this._log(message, 'INFO', 'cyan')
+  }
+
+  /**
+   * For printing service status logs.
+   * @param message Message to print.
+   */
+  public status(message: string): void {
+    this._log(message, 'STATUS', 'magenta')
+  }
+
+  /**
+   * For printing potentially interesting logs.
+   * @param message Message to print.
+   */
+  public interesting(message: string): void {
+    this._log(message, 'INFO', 'yellow')
+  }
+
+  /**
+   * For printing logs representing a success.
+   * @param message Message to print.
+   */
+  public success(message: string): void {
+    this._log(message, 'SUCCESS', 'green')
+  }
+
+  /**
+   * For printing logs representing an error.
+   * @param message Message to print.
+   */
+  public error(message: string | any[]): void {
+    // Backwards compatibility with previous logger
+    if (typeof message !== 'string') {
+      message = joinNewLines(...message)
+    }
+    this._log(message, 'ERROR', 'red')
+  }
+
+  /**
+   * For printing logs as a warning.
+   * @param message Message to print.
+   */
+  public warn(message: string): void {
+    this._log(message, 'WARNING', 'orange')
+  }
+
+  /**
+   * For printing debug logs.
+   * @param message Message to print.
+   */
+  public debug(message: string | any[]): void {
+    debug(this.namespace)(joinNewLines(...message))
+  }
+
+  /**
+   * Internal logging function.
+   * @param message Message to print.
+   * @param category Category to attach to the message.
+   * @param color Color to print the log with.
+   */
+  private _log(message: string, category: string, color: string): void {
+    // tslint:disable-next-line
+    console.log(
+      `${colors[color](`[${this.namespace}] [${category}]`)}: ${message}`
+    )
+  }
 }
 
 /**
+ * Forackwards compatibility:
  * Gets a logger specific to the provided identifier.
  *
  * @param identifier The identifier to use to tag log statements from this logger.
@@ -23,19 +99,14 @@ export const getLogger = (
   isTest: boolean = false,
   debugToUseTestOnly?: any
 ): Logger => {
-  const testString = isTest ? 'test:' : ''
-  return {
-    debug: getLogFunction(
-      `${testString}debug:${identifier}`,
-      debugToUseTestOnly
-    ),
-    info: getLogFunction(`${testString}info:${identifier}`, debugToUseTestOnly),
-    warn: getLogFunction(`${testString}warn:${identifier}`, debugToUseTestOnly),
-    error: getLogFunction(
-      `${testString}error:${identifier}`,
-      debugToUseTestOnly
-    ),
+  console.warn(`\`getLogger\` is now a legacy function and will soon be deprecated.
+                Please use \`new Logger(namespace)\` instead.`)
+
+  if (debugToUseTestOnly) {
+    console.warn(`Passing in a debugToUseTestOnly instance is now deprecated.`)
   }
+
+  return new Logger(`${isTest ? 'test:' : ''}${identifier}`)
 }
 
 export const logError = (logger: Logger, message: string, e: Error): void => {
@@ -43,6 +114,8 @@ export const logError = (logger: Logger, message: string, e: Error): void => {
     Error: ${e.message}. 
     Stack: ${e.stack}`)
 }
+
+export const LOG_NEWLINE_STRING = process.env.LOG_NEW_LINES ? '\n' : ' <\\n> '
 
 /**
  * Converts one or more items to log into a single line string.
@@ -61,24 +134,4 @@ const joinNewLines = (...logs: any[]): string => {
   }
 
   return stringifiedLogs.join(' ').replace(/\n/g, LOG_NEWLINE_STRING)
-}
-
-/**
- * Creates a debug instance with the provided identifier and wraps
- * its only function in a function that makes the strings to be logged a single
- * line before calling debug(identifier)(log).
- *
- * @param identifier The identifier used to prepend this log
- * @param debugToUseTestOnly The debug instance to use *should only be used for tests*
- * @returns The log function for the provided identifier
- */
-const getLogFunction = (
-  identifier: string,
-  debugToUseTestOnly: any = debug
-): any => {
-  const d = debugToUseTestOnly(identifier)
-  return (...logs: any[]): any => {
-    const singleLine = joinNewLines(...logs)
-    return d(singleLine)
-  }
 }
