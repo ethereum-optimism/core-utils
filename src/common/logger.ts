d5ca2f189ca8b366b8ae5848aac83c8a39e973e3
Liam Horne <liam@lihorne.com>
2021-03-08 12:30:53 -0800

    refactor: rewrite Logger class to maintain (message, object) argument ordering

--- src/common/logger.ts
+++ src/common/logger.ts
@@ -1,5 +1,12 @@
 import pino, { Logger as PinoLogger } from 'pino'
 
+export type LogLevel = 'trace' | 'debug' | 'info' | 'warn' | 'error' | 'fatal'
+
+export interface LoggerOptions {
+  name: string
+  level?: LogLevel
+}
+
 /**
  * Forackwards compatibility:
  * Gets a logger specific to the provided identifier.
@@ -21,23 +28,113 @@ export const getLogger = (
     console.warn(`Passing in a debugToUseTestOnly instance is now deprecated.`)
   }
 
-  return new Logger(identifier, isTest)
+  return new Logger({ name: identifier })
 }
 
 /**
  * Temporary wrapper class to maintain earlier module interface.
  */
 export class Logger {
-  constructor(namespace: string, isTest = false) {
-    return pino({
+  options: LoggerOptions
+  inner: pino.Logger
+
+  constructor(options: LoggerOptions) {
+    this.options = options
+
+    const loggerOptions = {
+      name: options.name,
+      level: options.level || 'debug',
+    }
+
+    pino({
+      name: options.name,
+
       // Pretty printing enabled by default to maintain expectations
       prettyPrint: {
         colorize: true,
       },
-    }).child({
-      module: namespace,
-      isTest,
     })
+
+    this.inner = pino(loggerOptions)
+  }
+
+  child(bindings: pino.Bindings): Logger {
+    const inner = this.inner.child(bindings)
+    const logger = new Logger(this.options)
+    logger.inner = inner
+    return logger
+  }
+
+  trace(msg: string, o?: object, ...args: any[]): void {
+    if (o) {
+      this.inner.trace(o, msg, ...args)
+    } else {
+      this.inner.trace(msg, ...args)
+    }
+  }
+
+  debug(msg: string, o?: object, ...args: any[]): void {
+    if (o) {
+      this.inner.debug(o, msg, ...args)
+    } else {
+      this.inner.debug(msg, ...args)
+    }
+  }
+
+  info(msg: string, o?: object, ...args: any[]): void {
+    if (o) {
+      this.inner.info(o, msg, ...args)
+    } else {
+      this.inner.info(msg, ...args)
+    }
+  }
+
+  warn(msg: string, o?: object, ...args: any[]): void {
+    if (o) {
+      this.inner.warn(o, msg, ...args)
+    } else {
+      this.inner.warn(msg, ...args)
+    }
+  }
+
+  warning(msg: string, o?: object, ...args: any[]): void {
+    if (o) {
+      this.inner.warn(o, msg, ...args)
+    } else {
+      this.inner.warn(msg, ...args)
+    }
+  }
+
+  error(msg: string, o?: object, ...args: any[]): void {
+    if (o) {
+      this.inner.error(o, msg, ...args)
+    } else {
+      this.inner.error(msg, ...args)
+    }
+  }
+
+  fatal(msg: string, o?: object, ...args: any[]): void {
+    if (o) {
+      this.inner.fatal(o, msg, ...args)
+    } else {
+      this.inner.fatal(msg, ...args)
+    }
+  }
+
+  crit(msg: string, o?: object, ...args: any[]): void {
+    if (o) {
+      this.inner.fatal(o, msg, ...args)
+    } else {
+      this.inner.fatal(msg, ...args)
+    }
+  }
+
+  critical(msg: string, o?: object, ...args: any[]): void {
+    if (o) {
+      this.inner.fatal(o, msg, ...args)
+    } else {
+      this.inner.fatal(msg, ...args)
+    }
   }
 }
 
